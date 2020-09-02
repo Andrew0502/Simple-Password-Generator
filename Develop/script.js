c13469327497376dfaf3c75d3c2d39305d88e436
Andrew0502 <agcmarcus@gmail.com>
2020-09-02 16:52:36 -0400

    Added files from gt-atl folder.

---
+++ Develop/script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function getPassword(){
+  // All YOUR CODE GOES HERE
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
