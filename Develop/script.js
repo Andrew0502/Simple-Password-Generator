75fd44252a3c04e7aecf99e6c1262a934dbb44d5
Andrew0502 <agcmarcus@gmail.com>
2020-09-02 22:54:15 -0400

    "Fixed final if statement"

--- Develop/script.js
+++ Develop/script.js
@@ -13,21 +13,19 @@ function generatePassword(){
   // All YOUR CODE GOES HERE
   var finalPass = "";
   var howLong = prompt("How long would you like your password to be?", 
-  "(max length 128 characters)");
+  "(Pick a number between 8 and 128.)");
+
+
   var lowerCase = confirm("Would you like to include lower case letters?");
   var upperCase = confirm("Would you like to include upper case letters? ");
   var numbers = confirm("Would you like to include numbers? ");
   var characters = confirm("Would you like to include characters?");
-
-  console.log("test-1");
 
   var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowers = "abcdefghijklmnopqrstuvwxyz";
   var chars = "!@#$%^&*()";
   var numbs = "1234567890"; 
   var selected = "";
-
-  console.log("test-2")
 
   if(lowerCase === true){
     selected = selected + lowers;
@@ -45,8 +43,8 @@ function generatePassword(){
    selected = selected + chars;
   }
 
-  for( i = 0; i < howLong.length; i++){
-    var randomChar = Math.floor(Math.random(selected)* howLong);
+  for(var i = 0; i < howLong; i++){
+    var randomChar = Math.floor(Math.random()* selected.length);
     finalPass = finalPass + selected[randomChar];
 
      // var num = math.floor(math.random() * 10) + 1;
